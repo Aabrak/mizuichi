1495e5b70fa1b331675819b243c24c4147077e77
Aabrak <sebapaun@gmail.com>
2022-05-16 14:50:16 +0200

    update from laptop

--- main.js
+++ main.js
@@ -1,183 +1,59 @@
-// IMPORT SHADERS --------------------------------------------------------------------------------------
+// IMPORT SHADERS
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 
-// VARS --------------------------------------------------------------------------------------
-const kobsTableRatio = 1.4;
-const kobsTableSize = 20;
+// VARS
 const camfov = 45;
-var runtime = 0;
-let camVels = {
-  x: 0.1,
-  y: 0.1,
-  z: 0.1
-};
 
-// FUNCS
-let range = (min, max) => {
-  return Math.random() * (max - min) + min;
-}
-
-// SCENE --------------------------------------------------------------------------------------
+// SCENE
 const scene = new THREE.Scene();
 
-// CAMERA --------------------------------------------------------------------------------------
+// CAMERA
 const camera = new THREE.PerspectiveCamera( camfov, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.x = kobsTableSize/1.6;
-camera.position.y = kobsTableSize/1.5 + kobsTableSize*1.3;
-camera.position.z = 3.2*kobsTableSize;
+camera.position.z = 5;
 
-
-// RENDERER --------------------------------------------------------------------------------------
+// RENDERER
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// LIGHTING --------------------------------------------------------------------------------------
+// LIGHTING
 var AmbiLight = new THREE.AmbientLight(0xffffff, 0.5);
 var PoniLight = new THREE.PointLight(0xffffff, 0.5);
 PoniLight.position.set( 50, 50, 50 );
 scene.add(AmbiLight);
 scene.add(PoniLight);
 
-
-// OBJECTS --------------------------------------------------------------------------------------
-// SHADER STUFF
-let glowColor = new THREE.Vector3(0, 0.7, 1);
+// OBJECT
+let glowColor = new THREE.Vector3(0, 0, 1);
 let uniforms = {
-  "s":   { type: "f", value: -1.0},
-  "b":   { type: "f", value: 1.0},
-  "p":   { type: "f", value: 2.0 },
-  glowColor: { type: "c", value: glowColor },
+  glowColor: { value: glowColor },
   sneeze: { value: 0.1 },
   time: { value: 1.0 }
 }
-
-// GEOMETRIES --------------------------------------------------------------------------------------
-let geoBox = new THREE.BoxGeometry()
-
-// MATERIALS --------------------------------------------------------------------------------------
-// AIR MAT
-let matboundingbox =  new THREE.MeshPhongMaterial({
-  color: 0xffffff,
-  transparent: true,
-  opacity: 0.1
-  // wireframe: true
-})
-// AIR MAT
-let mat0 =  new THREE.MeshStandardMaterial({
-  color: 0x0088ff,
-  visible: false,
-  wireframe: true
-})
-// PHYS MAT
-let mat1 =  new THREE.ShaderMaterial({
+let geometry = new THREE.SphereGeometry()
+let material =  new THREE.ShaderMaterial({
   vertexShader,
   fragmentShader,
   uniforms: uniforms,
-  wireframe: false
+  wireframe: true
 })
+let c1 = new THREE.Mesh(geometry, material)
+c1.rotation.x = 10
+scene.add(c1)
 
-// MESHES --------------------------------------------------------------------------------------
-// BOUNDING BOX SCUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED
-let boundbox = new THREE.Mesh(geoBox, matboundingbox)
-boundbox.scale.x = kobsTableSize*kobsTableRatio;
-boundbox.scale.y = kobsTableSize*kobsTableRatio;
-boundbox.scale.z = kobsTableSize*kobsTableRatio;
-boundbox.position.x = kobsTableSize/2*kobsTableRatio/1.05;
-boundbox.position.y = kobsTableSize/2*kobsTableRatio/1.05;
-boundbox.position.z = kobsTableSize/2*kobsTableRatio/1.05;
-scene.add(boundbox)
-
-// ARRAY
-let kobsTable = [];
-let valueTable = [];
-
-for (let i = 0; i < kobsTableSize; i++) {
-  kobsTable[i] = [];
-  valueTable[i] = [];
-
-  for (let j = 0; j < kobsTableSize; j++) {
-    kobsTable[i][j] = [];
-    valueTable[i][j] = [];
-
-    for (let k = 0; k < kobsTableSize; k++) {
-      kobsTable[i][j].push(new THREE.Mesh(geoBox, mat0));
-      scene.add(kobsTable[i][j][k])
-      valueTable[i][j][k] = 0;
-      
-      kobsTable[i][j][k].position.x = i*kobsTableRatio;
-      kobsTable[i][j][k].position.y = j*kobsTableRatio;
-      kobsTable[i][j][k].position.z = k*kobsTableRatio;
-    }
-    
-  }
-  
-}
-console.log(kobsTable)
-console.log("kobs up vals down")
-console.log(valueTable)
-valueTable[kobsTableSize-1][kobsTableSize-1][kobsTableSize-1] = 1;
-
-camera.lookAt(kobsTable[Math.floor(kobsTableSize/2)][Math.floor(kobsTableSize/2)][Math.floor(kobsTableSize/2)].position)
+console.log(c1);
 
 // MAIN LOOP
 function animate() {
   requestAnimationFrame(animate);
-  runtime++;
-  // console.log(runtime);
 
-  for (let i = 0; i < kobsTableSize; i++) {
-    for (let j = 0; j < kobsTableSize; j++) {
-      for (let k = 0; k < kobsTableSize; k++) {
-        switch (valueTable[i][j][k]) {
-          case 0: //air
-            kobsTable[i][j][k].material = mat0;
-            break;
+  c1.rotation.x += 0.001;
+  c1.rotation.y += 0.002;
+  // glowColor.x = Math.sin(c1.rotation.x) * 0.5 + 0.5;
+  // glowColor.y = Math.sin(c1.rotation.y) * 0.5 + 0.5;
+  // glowColor.z = Math.sin(c1.rotation.z) * 0.5 + 0.5;
 
-          case 1: //phys
-            kobsTable[i][j][k].material = mat1;
-            // check if air under
-            if(!(runtime % 5)) {
-              if(j > 0 && valueTable[i][j-1][k] == 0) {
-                valueTable[i][j][k] = 0;
-                valueTable[i][j-1][k] = 1;
-              // check if at bottom
-              } else if(i == kobsTableSize-2 && j == kobsTableSize-2 && k == kobsTableSize-2) {
-                break;
-                // check if air east
-              } else if(i > 0 && valueTable[i-1][j][k] == 0) {
-                valueTable[i][j][k] = 0;
-                valueTable[i-1][j][k] = 1;
-                // check if air east
-              } else if(valueTable[i+1][j][k] == 0) {
-                valueTable[i][j][k] = 0;
-                valueTable[i+1][j][k] = 1;
-                // check if air north
-              } else if(valueTable[i][j][k-1] == 0) {
-                valueTable[i][j][k] = 0;
-                valueTable[i][j][k-1] = 1;
-                // check if air south
-              } else if(valueTable[i][j][k+1] == 0) {
-                valueTable[i][j][k] = 0;
-                valueTable[i][j][k+1] = 1;
-              }
-            }
-            break;
-        }
-      }
-    }
-  }
-
-  if(!(runtime % 10)) {
-    valueTable[Math.floor(range(0, kobsTableSize))][kobsTableSize-1][Math.floor(range(0, kobsTableSize))] = 1;
-  }
-
-  if(camera.position.x > kobsTableSize*2 || camera.position.x < -kobsTableSize) camVels.x = -camVels.x
-  if(camera.position.z > kobsTableSize*2 || camera.position.z < -kobsTableSize) camVels.z = -camVels.z
-  camera.position.x += camVels.x;
-  camera.position.z += camVels.z;
-  camera.lookAt(kobsTable[Math.floor(kobsTableSize/2)][Math.floor(kobsTableSize/2)][Math.floor(kobsTableSize/2)].position)
-
+  uniforms[ 'time' ].value = performance.now();
   renderer.render(scene, camera);
 }; animate();
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,6 +1,6 @@
 varying vec3 vNormal;
 varying vec3 vPositionNormal;
-uniform float sneeze;
+varying float scale;
 
 float rand(float n){return fract(sin(n) * 43758.5453123);}
 
@@ -15,7 +15,8 @@ void main()
   vNormal = normalize( normalMatrix * normal );
   vPositionNormal = normalize(( modelViewMatrix * vec4(position, 1.0) ).xyz);
 
-  float displacement = sneeze * rand(2.0);
-  vec3 newPosition = position + normal * displacement;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  scale = 1.0;
+
+  vec3 newPosition = position + normal * noise(10.0) * scale;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
 }
